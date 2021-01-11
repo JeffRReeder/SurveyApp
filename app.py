637007f595b33ec04be936ebb03463bc328bd012
JeffRReeder <43863681+JeffRReeder@users.noreply.github.com>
2021-01-11 14:14:31 -0800

    changed pring statement

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-print("hello")
+print("hello world")
 
 # Store answers in this list (ex ['Yes', 'No', 'Less than $10,000', 'Yes'])
 responses = []
