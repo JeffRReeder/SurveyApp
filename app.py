adb163a61721bbf54137b5f612026ec63dfeb5d3
JeffRReeder <43863681+JeffRReeder@users.noreply.github.com>
2021-01-18 13:51:01 -0800

    created templates

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
-print("hello world")
+# the toolbar is only enabled in debug mode:
+app.debug = True
+
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config['SECRET_KEY'] = 'key'
+
+toolbar = DebugToolbarExtension(app)
 
 # Store answers in this list (ex ['Yes', 'No', 'Less than $10,000', 'Yes'])
 responses = []
+
+
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+@app.route('/questions/0')
+def question():
+    return render_template("questions.html")
